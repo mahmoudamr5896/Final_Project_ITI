ae0de3316f7024e6e7ffdffb2921ead09a527f91
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-02-10 18:23:37 +0000

    merg

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+                      import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,14 +7,9 @@ import Register from './Pages/Regster';
 function App() {
   return (
     <div >
-        <BrowserRouter  >
-        <Switch>
-          <Route exact path='/' component={HomePage} ></Route>
-          <Route exact path='/register' component={Register}></Route>
-        </Switch>
-        </BrowserRouter>
+    
     </div>
   );
-}
+}                                                                             
 
 export default App;
