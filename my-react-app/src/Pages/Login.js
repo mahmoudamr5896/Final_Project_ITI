bd453c62b9877c60f994eab6596a84eee540ab30
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-02-11 12:36:30 +0000

    save

--- my-react-app/src/Pages/Login.js
+++ my-react-app/src/Pages/Login.js
@@ -63,8 +63,10 @@ const handleConfirmPasswordChange = (event) => {
                     <div className="col-12 col-sm-10 col-md-12 col-lg-11 col-xl-10">
                         <div className="card1 d-flex mx-auto my-5">
                             <div className="row">
-                                <div className="col-md-5 col-sm-12 col-xs-12 c1 p-5">
-                                    <div className="row mb-5 m-3"> <img src="https://i.imgur.com/pFfTOwy.jpg" width="70vw" height="55vh" alt=""/> </div> <img src={imageSrc} width="120vw" height="210vh" className="mx-auto d-flex" alt="Teacher"/>
+                                <div className="col-md-5 col-sm-12 col-xs-12 c1 p-5 bg-primary">
+                                    <div className="row mb-5 m-3">
+                                     {/* <img src="https://i.imgur.com/pFfTOwy.jpg" width="70vw" height="55vh" alt=""/>  */}
+                                     </div> <img src={imageSrc} width="120vw" height="210vh" className="mx-auto d-flex" alt="Teacher"/>
                                     <div className="row justify-content-center">
                                         <div className="w-75 mx-md-5 mx-1 mx-sm-2 mb-5 mt-4 px-sm-5 px-md-2 px-xl-1 px-2">
                                             <h1 className="wlcm">Welcome to your blackboard</h1> <span className="sp1"> <span className="px-3 bg-danger rounded-pill"></span> <span className="ml-2 px-1 rounded-circle"></span> <span className="ml-2 px-1 rounded-circle"></span> </span>
