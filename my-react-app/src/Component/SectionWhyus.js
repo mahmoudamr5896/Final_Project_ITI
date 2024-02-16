7f980f2124573390a42f694db7a592c066d452d2
maii-zain <maim90284@gmail.com>
2024-02-16 04:30:04 +0200

    choose user type responsive

--- my-react-app/src/Component/SectionWhyus.js
+++ my-react-app/src/Component/SectionWhyus.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './CSS/style.css'
+import './CSS/style.css';
 
 function FeaturesSection() {
     return (
@@ -7,12 +7,12 @@ function FeaturesSection() {
             <div className="container feature px-lg-0">
                 <div className="row g-0 mx-lg-0">
                     <div className="col-lg-6 feature-text py-5 wow fadeIn" data-wow-delay="0.1s">
-                        <div className="p-lg-5 ps-lg-0 ">
+                        <div className="p-lg-5 ps-lg-0">
                             <h1 className="text-white mb-4">Why Choose Us</h1>
                             <p className="text-white mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo erat amet</p>
-                            <div className="row g-4" >
-                                <div className="col-6" >
-                                    <div className="d-flex align-items-center" >
+                            <div className="row g-4">
+                                <div className="col-md-6">
+                                    <div className="d-flex align-items-center">
                                         <div className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style={{ width: '55px', height: '55px' }}>
                                             <i className="fa fa-user-md "></i>
                                         </div>
@@ -22,7 +22,7 @@ function FeaturesSection() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-6" >
+                                <div className="col-md-6">
                                     <div className="d-flex align-items-center">
                                         <div className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style={{ width: '55px', height: '55px'}}>
                                             <i className="fa fa-check "></i>
@@ -33,7 +33,7 @@ function FeaturesSection() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-6" >
+                                <div className="col-md-6">
                                     <div className="d-flex align-items-center">
                                         <div className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style={{ width: '55px', height: '55px'}}>
                                             <i className="fa fa-comment-medical "></i>
@@ -44,7 +44,7 @@ function FeaturesSection() {
                                         </div>
                                     </div>
                                 </div>
-                                <div className="col-6" >
+                                <div className="col-md-6">
                                     <div className="d-flex align-items-center">
                                         <div className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style={{ width: '55px', height: '55px'}}>
                                             <i className="fa fa-headphones "></i>
@@ -58,9 +58,9 @@ function FeaturesSection() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-6 pe-lg-0 wow fadeIn" data-wow-delay="0.5s" style={{ minHeight: '400px' }}>
+                    <div className="col-lg-6 pe-lg-0 wow fadeIn" data-wow-delay="0.5s">
                         <div className="position-relative h-100">
-                            <img className="position-absolute img-fluid w-100 h-100" src='https://www.beaumont.org/images/default-source/primary-care/getting-to-know-doctor.jpg?sfvrsn=cc08ede2_0' style={{ objectFit: 'cover' }} alt="" />
+                            <img className="position-absolute img-fluid w-100 h-100" src='https://www.beaumont.org/images/default-source/primary-care/getting-to-know-doctor.jpg?sfvrsn=cc08ede2_0' style={{ objectFit: 'cover', minHeight: '400px' }} alt="" />
                         </div>
                     </div>
                 </div>
