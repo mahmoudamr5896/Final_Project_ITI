606490ad0a60cf4960c2826b346df7d1842bcdc3
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-02-20 12:26:15 +0000

    save

--- my-react-app/src/Pages/Doctorshow.js
+++ my-react-app/src/Pages/Doctorshow.js
@@ -97,31 +97,12 @@ const selectLocation = () => {
 const [ExperienceData, setExperienceData] = useState(null);
 const Select_Exprience = ()=>{
 const data=(
-  <>
-     <h1 className="text-start col-12">
+  <div className="container">     <h1 className="text-start col-12">
      Experience and Background Checks
      <hr></hr>
    </h1>
-   
    <br></br>
-   <div className="row border " style={{width:'100%',height:'100px'}}>
-     <span className="">
-   <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTwc96ABQRwexDILOeixZ0SMISPPeoavW7U1sJfIfX-vwfDL_Xc' style={{width:'70px'}} />
-   <p className="">Exprience</p></span>
-   <span className="col-6" style={{width:'100px'}}>
-     <img src='https://media.licdn.com/dms/image/D4E0BAQGMXphjW3D52g/company-logo_200_200/0/1703668545508?e=1715817600&v=beta&t=33f6xNyyb4747yocqYoswXfCk5AiBYdAoC-GQQLBzLs' style={{width:'70px'}}/>
-     <span><p className="">Background</p></span></span>
-   </div>
-   <p className="text-start">Experience Check<br></br>
-     Check Dr.{doctorInfo.name}'s experience treating your condition or procedure</p>
-   <div className="container">
-   <div className="container ">
-         <h1 className="text-start ">
-       Experience and Background Checks
-       <hr />
-     </h1>
-   </div>
-   <div className="container mt-5">
+    <div className="container mt-5">
          <div className="row border mb-5" style={{ width: '500px', height: 'auto' }}>
            <div className="col-6 d-flex align-items-center">
                <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTwc96ABQRwexDILOeixZ0SMISPPeoavW7U1sJfIfX-vwfDL_Xc' style={{ width: '70px' }} />
@@ -136,9 +117,27 @@ const data=(
            Check Dr. {doctorInfo.name}'s experience treating your condition or procedure
        </p>
    </div>
-   </div>
+  {/* <div className="row border " style={{width:'100%',height:'100px'}}>
+            <span className="">
+          <img src='https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTwc96ABQRwexDILOeixZ0SMISPPeoavW7U1sJfIfX-vwfDL_Xc' style={{width:'70px'}} />
+          <p className="">Exprience</p></span>
+          <span className="col-6" style={{width:'100px'}}>
+            <img src='https://media.licdn.com/dms/image/D4E0BAQGMXphjW3D52g/company-logo_200_200/0/1703668545508?e=1715817600&v=beta&t=33f6xNyyb4747yocqYoswXfCk5AiBYdAoC-GQQLBzLs' style={{width:'70px'}}/>
+            <span><p className="">Background</p></span></span>
+  </div> */}
+          {/* <p className="text-start">Experience Check<br></br>
+            Check Dr.{doctorInfo.name}'s experience treating your condition or procedure</p>
+          <div className="container">
+          <div className="container ">
+                <h1 className="text-start ">
+              Experience and Background Checks
+              <hr />
+            </h1>
+          </div> </div> */}
+  
+            
 
-  </>
+  </div>
 
 )
 setIsEditProfileOpen(null);
@@ -324,7 +323,7 @@ const Select_Appon = ()=>{
     <div class="row g-5">
         <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
             <p class="d-inline-block border rounded-pill py-1 px-4">Appointment</p>
-            <h1 class="mb-4">Make An Appointment To Visit Our Doctor</h1>
+            <h1 class="mb-4">Make An Appointment To Visit DR./{doctorInfo.Doctor_Name}</h1>
             <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
             <div class="bg-light rounded d-flex align-items-center p-5 mb-4">
                 <div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-white" style={{width: "55px;", height: "55px;"}}>
@@ -413,7 +412,7 @@ const Select_Appon = ()=>{
     </div>
                             {!payNow ?  (
                             <>
-                              <CheckoutForm></CheckoutForm>
+                              {/* <CheckoutForm></CheckoutForm> */}
                             </>
                         ):(<></>)}
 </div>
@@ -426,7 +425,6 @@ const Select_Appon = ()=>{
   setLocationData(null)
   setRatingData(null)
 }
-
 const Select_Overview = ()=>{
 
 }
@@ -451,7 +449,6 @@ useEffect(() => {
       console.error('Error fetching user data:', error);
     });
 }, [id]);
-
 const handleChange = (e) => {
   const { name, value } = e.target;
   setDoctorInfo(prevData => ({
@@ -459,7 +456,6 @@ const handleChange = (e) => {
     [name]: value
   }));
 };
-
 const handleSubmit = (e) => {
   e.preventDefault();
   axios.patch(`https://retoolapi.dev/EBWb8G/Doctors/${doctorInfo.id}`, doctorInfo)
@@ -470,7 +466,6 @@ const handleSubmit = (e) => {
       console.error('Error updating user data:', error);
     });
 };
-
 const [isEditProfileOpen, setIsEditProfileOpen] = useState(null);
 const toggleEditProfile = () => {
   const data=(
@@ -520,7 +515,6 @@ const toggleEditProfile = () => {
   setLocationData(null)
   setRatingData(null)
 };
-
 //___________________________________________________________________
 
 //__________________________________________________________________________________________________________________
@@ -534,7 +528,6 @@ const[problemDescription,setproblemDescription]=useState('')
 const handlePayNowChange = (event) => {
     setPayNow(event.target.value === 'no');
 };
-
 const handlePaymentMethodChange = (event) => {
     setPaymentMethod(event.target.value);
 };
@@ -583,9 +576,6 @@ axios
     console.error('Error posting Appointment:', error);
   }); 
 }
-
-
-
 //________________________________________________________________________________
 const [userData, setUserData] = useState(null);
 useEffect(() => {
@@ -595,6 +585,7 @@ useEffect(() => {
     setUserData(JSON.parse(storedUserData));
   }
 }, []);
+//_______________________________________________________________________________________________________
   return ( 
       <>
         <div className="container-fluid">
