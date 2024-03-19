6e50bb597e2f89a4ad76aed80ba3a0d8e4c064d2
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-03-19 01:42:53 +0000

    added integration

--- my-react-app/src/Component/Schule.js
+++ my-react-app/src/Component/Schule.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function DoctorScheduleForm({doctorId}) {
-  // Define initial state for the schedule
+function DoctorScheduleForm({ doctorId }) {
   const [selectedDay, setSelectedDay] = useState('');
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
   const [doctorSchedule, setDoctorSchedule] = useState([]);
+  const [editingScheduleItemId, setEditingScheduleItemId] = useState(null);
 
   useEffect(() => {
-    // Fetch the doctor's schedule
     axios.get(`http://127.0.0.1:8000/availabilities/?doctor=${doctorId}`)
       .then(response => {
         setDoctorSchedule(response.data);
@@ -17,121 +16,200 @@ function DoctorScheduleForm({doctorId}) {
       .catch(error => {
         console.error('Error fetching doctor schedule:', error);
       });
-  }, [doctorSchedule]);
+  }, [doctorId]);
 
   const storedId = sessionStorage.getItem('userData');
   const userDatas = JSON.parse(storedId);
 
-  // Handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Check if both start and end times are selected
     if (!startTime || !endTime) {
       alert('Please select both start and end times.');
       return;
     }
 
-    // Post the schedule data for the selected day
-    axios.post(`http://127.0.0.1:8000/availabilities/`, {
+    const scheduleData = {
       doctor: userDatas.id,
       day: selectedDay,
       start_time: startTime,
       end_time: endTime
-    })
+    };
+
+    if (editingScheduleItemId) {
+      // If editing a schedule item, update it instead of adding a new one
+      axios.put(`http://127.0.0.1:8000/availabilities/${editingScheduleItemId}/`, scheduleData)
+        .then(response => {
+          console.log('Schedule item updated successfully:', response.data);
+          setEditingScheduleItemId(null); // Clear the editing schedule item ID
+          // Fetch the updated doctor's schedule after updating
+          axios.get(`http://127.0.0.1:8000/availabilities/?doctor=${doctorId}`)
+            .then(response => {
+              setDoctorSchedule(response.data);
+            })
+            .catch(error => {
+              console.error('Error fetching doctor schedule after update:', error);
+            });
+        })
+        .catch(error => {
+          console.error('Error updating schedule item:', error);
+        });
+    } else {
+      // If not editing, add a new schedule item
+      axios.post(`http://127.0.0.1:8000/availabilities/`, scheduleData)
+        .then(response => {
+          console.log('Schedule item added successfully:', response.data);
+          // Fetch the updated doctor's schedule after addition
+          axios.get(`http://127.0.0.1:8000/availabilities/?doctor=${doctorId}`)
+            .then(response => {
+              setDoctorSchedule(response.data);
+            })
+            .catch(error => {
+              console.error('Error fetching doctor schedule after addition:', error);
+            });
+        })
+        .catch(error => {
+          console.error('Error adding schedule item:', error);
+        });
+    }
+
+    // Reset form fields after submission
+    setSelectedDay('');
+    setStartTime('');
+    setEndTime('');
+  };
+
+  const handleDelete = (id) => {
+    axios.delete(`http://127.0.0.1:8000/availabilities/${id}/`)
       .then(response => {
-        console.log('Schedule saved successfully:', response.data);
-        // Optionally, you can redirect the doctor to another page or perform other actions upon successful submission
+        console.log('Schedule item deleted successfully:', id);
+        // Fetch the updated doctor's schedule after deletion
+        axios.get(`http://127.0.0.1:8000/availabilities/?doctor=${doctorId}`)
+          .then(response => {
+            setDoctorSchedule(response.data);
+          })
+          .catch(error => {
+            console.error('Error fetching doctor schedule after deletion:', error);
+          });
       })
       .catch(error => {
-        console.error('Error saving schedule:', error);
-        // Handle error if necessary
+        console.error('Error deleting schedule item:', error);
       });
+  };
+
+  const handleEdit = (id) => {
+    // Find the schedule item to be edited based on its ID
+    const scheduleItemToEdit = doctorSchedule.find(item => item.id === id);
+    if (scheduleItemToEdit) {
+      // Populate the form fields with the data of the schedule item to be edited
+      setSelectedDay(scheduleItemToEdit.day);
+      setStartTime(scheduleItemToEdit.start_time);
+      setEndTime(scheduleItemToEdit.end_time);
+      // Set the ID of the editing schedule item
+      setEditingScheduleItemId(id);
+    }
   };
 
   return (
     <div className="doctor-schedule-form">
-      {userDatas && userDatas.role === 'Doctor' && (<form onSubmit={handleSubmit} className="schedule-form">
-        <table>
-            <tr>
-        <td>
-        <div className="form-group">
-          <label htmlFor="day">Day:</label>
-          <select
-            id="day"
-            value={selectedDay}
-            onChange={(e) => setSelectedDay(e.target.value)}
-            required
-          >
-            <option value="">Select Day</option>
-            <option value="Monday">Monday</option>
-            <option value="Tuesday">Tuesday</option>
-            <option value="Wednesday">Wednesday</option>
-            <option value="Thursday">Thursday</option>
-            <option value="Friday">Friday</option>
-            <option value="Saturday">Saturday</option>
-            <option value="Sunday">Sunday</option>
-          </select>
-        </div>
-        </td>
-            <td>  
-            <div className="form-group">
-          <label htmlFor="startTime">Start Time:</label>
-          <input
-            type="time"
-            id="startTime"
-            value={startTime}
-            onChange={(e) => setStartTime(e.target.value)}
-            required
-          />
-        </div>
-
-            </td>
-          <td>
-          <div className="form-group">
-          <label htmlFor="endTime">End Time:</label>
-          <input
-            type="time"
-            id="endTime"
-            value={endTime}
-            onChange={(e) => setEndTime(e.target.value)}
-            required
-          />
-        </div>
-          </td>
-            </tr>
-            <button type="submit" className="btn-submit">Save</button>
-        </table>
-      </form>
-        )}
+      {userDatas && userDatas.role === 'Doctor' && (
+        <form onSubmit={handleSubmit} className="schedule-form">
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <div className="form-group">
+                    <label htmlFor="day">Day:</label>
+                    <select
+                      id="day"
+                      value={selectedDay}
+                      onChange={(e) => setSelectedDay(e.target.value)}
+                      required
+                    >
+                      <option value="">Select Day</option>
+                      <option value="Monday">Monday</option>
+                      <option value="Tuesday">Tuesday</option>
+                      <option value="Wednesday">Wednesday</option>
+                      <option value="Thursday">Thursday</option>
+                      <option value="Friday">Friday</option>
+                      <option value="Saturday">Saturday</option>
+                      <option value="Sunday">Sunday</option>
+                    </select>
+                  </div>
+                </td>
+                <td>
+                  <div className="form-group">
+                    <label htmlFor="startTime">Start Time:</label>
+                    <input
+                      type="time"
+                      id="startTime"
+                      value={startTime}
+                      onChange={(e) => setStartTime(e.target.value)}
+                      required
+                    />
+                  </div>
+                </td>
+                <td>
+                  <div className="form-group">
+                    <label htmlFor="endTime">End Time:</label>
+                    <input
+                      type="time"
+                      id="endTime"
+                      value={endTime}
+                      onChange={(e) => setEndTime(e.target.value)}
+                      required
+                    />
+                  </div>
+                </td>
+              </tr>
+              <button type="submit" className="btn-submit">Save</button>
+            </tbody>
+          </table>
+        </form>
+      )}
       <div className="container">
-
-  <div className="row" style={{width:'800px'}}>
-    <div className="col">
-      <h2 className="mt-4 mb-3">Doctor's Schedule</h2>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Day</th>
-            <th>Start Time</th>
-            <th>End Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {doctorSchedule.map((scheduleItem, index) => (
-            <tr key={index}>
-              <td>{scheduleItem.day}</td>
-              <td>{scheduleItem.start_time}</td>
-              <td>{scheduleItem.end_time}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  </div>
-
-</div>
-
+        <div className="row" style={{ width: '800px' }}>
+          <div className="col">
+            <h2 className="mt-4 mb-3">Doctor's Schedule</h2>
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th>Day</th>
+                  <th>Start Time</th>
+                  <th>End Time</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {doctorSchedule.map((scheduleItem, index) => (
+                  <tr key={index}>
+                    <td>{scheduleItem.day}</td>
+                    <td>{scheduleItem.start_time}</td>
+                    <td>{scheduleItem.end_time}</td>
+                    <td>
+                      <button
+                        className="btn btn-sm btn-primary me-1"
+                        onClick={() => handleEdit(scheduleItem.id)}
+                      >
+                        Edit
+                      </button>
+                      </td>
+                      <td>
+                      <button
+                        className="btn btn-sm btn-danger"
+                        onClick={() => handleDelete(scheduleItem.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
