ca857ef43feeb648bd6d6b9357c0cf313deae618
nardeenNathan123 <nardeennathan@gmail.com>
2024-03-24 06:35:13 +0200

    m

--- my-react-app/src/Pages/RegNut.js
+++ my-react-app/src/Pages/RegNut.js
@@ -68,14 +68,6 @@ function RegsNut() {
   }, []);
 
   const [error, setError] = useState('');
-
-  // const handleImageChange = (e) => {
-  //   setFormData({
-  //     ...formData,
-  //     image: e.target.files[0], // Set the selected image file
-  //   });
-  // };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const newErrors = { ...errors };
@@ -94,7 +86,7 @@ function RegsNut() {
   
       const user_n = id_.find((user) => user.Email === formData.email);
       if (user_n) {
-        setError('This Email Is Exites');
+        setError('The username already exists');
       } else {
         const postData = new FormData();
         postData.append('username', formData.userName);
@@ -129,11 +121,11 @@ function RegsNut() {
                 history.push('/logNut');
               })
               .catch((error) => {
-                setError('Error posting Doctor:', error);
+                setError('User name is already exist:', error);
               });
           })
           .catch((error) => {
-            setError('Error posting User:', error);
+            setError('User name is already exist:', error);
           });
       }
     }
@@ -201,7 +193,8 @@ function RegsNut() {
                     placeholder="Email"
                     value={formData.email}
                     onChange={handleChange}
-                  />
+                 
+        />
                   <span className="error" style={{ color: 'red', textAlign: 'left', display: 'block' }}>{errors.email}</span>
                 </div>
                 <div className="form-group">
@@ -226,9 +219,7 @@ function RegsNut() {
                   />
                   <span className="error" style={{ color: 'red', textAlign: 'left', display: 'block' }}>{errors.repeatPassword}</span>
                 </div>
-                {/* <div className="form-group">
-                  <input type="file" name="image" onChange={handleChange} />
-                </div> */}
+                
                 <button type="submit">Sign Up</button>
                 <p className="text-danger">{error}</p>
               </form>
@@ -247,116 +238,3 @@ function RegsNut() {
 }
 
 export default RegsNut;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-// function EditUserPage({ userId }) {
-//   const [userData, setUserData] = useState({
-//     name: "",
-//     age: 0,
-//     image: '',
-//     experience: 0,
-//     gender: "",
-//     phone: "",
-//     location: ""
-//   });
-  
-//   useEffect(() => {
-//     axios.get(`http://127.0.0.1:8000/doctors/${userId}/`)
-//       .then(response => {
-//         setUserData(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching user data:', error);
-//       });
-//   }, [userId]);
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setUserData(prevData => ({
-//       ...prevData,
-//       [name]: value
-//     }));
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     axios.patch(`http://127.0.0.1:8000/doctors/${userId}/`, userData)
-//       .then(response => {
-//         console.log('User data updated successfully:', response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error updating user data:', error);
-//       });
-//   };
-
-//   return (
-//     <div className='container m-5'>
-//       <h1>Edit User</h1>
-//       <form onSubmit={handleSubmit}>
-//         <div className="mb-3">
-//           <label htmlFor="password" className="form-label">Password:</label>
-//           <input id="password" type="password" name="age" className="form-control" value={userData.age} onChange={handleChange} />
-//         </div>
-//         <div className="mb-3">
-//           <label htmlFor="img" className="form-label">Img:</label>
-//           <input id="img" type="text" name="image" className="form-control" value={userData.image} onChange={handleChange} />
-//         </div>
-//         <div className="mb-3">
-//           <label htmlFor="name" className="form-label">Name:</label>
-//           <input id="name" type="text" name="name" className="form-control" value={userData.name} onChange={handleChange} />
-//         </div>
-//         <div className="mb-3">
-//           <label htmlFor="email" className="form-label">Email:</label>
-//           <input id="email" type="text" name="experience" className="form-control" value={userData.experience} onChange={handleChange} />
-//         </div>
-//         <div className="mb-3">
-//           <label htmlFor="phone" className="form-label">Phone:</label>
-//           <input id="phone" type="text" name="phone" className="form-control" value={userData.phone} onChange={handleChange} />
-//         </div>
-//         <div className="mb-3">
-//           <label htmlFor="location" className="form-label">Location:</label>
-//           <input id="location" type="text" name="location" className="form-control" value={userData.location} onChange={handleChange} />
-//         </div>
-//         <button type="submit" className="btn btn-primary">Save Changes</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default EditUserPage;
